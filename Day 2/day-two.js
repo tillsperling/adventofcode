75f3cef61d0694b49fee5f60efa45e24f6f659a0
tillherpertz <tillh@live.de>
2022-12-05 23:46:23 +0100

    renamed dayone, added day-two files

---
+++ Day 2/day-two.js
@@ -0,0 +1,25 @@
+/* 
+
+Enemy
+
+    A = Rock
+    B = Paper
+    C = Scissor
+
+You
+
+    X = Rock 1
+    Y = Paper 2
+    Z = Scissor 3
+
+Points
+
+    Rock 1
+    Paper 2
+    Scissor 3
+
+    Loss 0
+    Draw 3
+    Win 6
+
+*/
