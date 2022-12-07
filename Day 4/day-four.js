560f6010278eb98a005a494484dfac379f7782b8
tillherpertz <tillh@live.de>
2022-12-07 11:16:51 +0100

    visualized solution

--- Day 4/day-four.js
+++ Day 4/day-four.js
@@ -1,12 +1,52 @@
 /*
 
-Every Secition is an ID
+Every section is an ID
 Each Elv has a range of IDs assigned
-Many Assignments overlap though
+Many assignments overlap though
 Elves pair up in twos 
 The assignments range from a single section to multiple sections
 
 Find out which pair as an assignment completely contained in the other ones.
 Count these.
 
+Thoughts:
+
+each elv has 2 values, so 4 per pair
+
+elv1 a b
+elv2 c d
+
+pair is only inside if 
+c >= a, d <= b
+a >= c, b <= d
+
+True  
+<-a------------b->
+    <-c-----d->
+
+True
+    <-a-----b->
+<-c------------d->
+
+True
+<-a------------b->
+<-c------------d->
+
+
+False
+<-a--b-> 
+        <-c--d->
+
+False
+        <-a--b->
+<-c--d->
+
+False
+    <-a-----b->
+<-c-----d->
+
+False
+<-a-----b->
+    <-c-----d->
+    
 */
