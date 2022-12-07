c5b4a409d2ac380188ca364f5cf379ddd0bafad3
tillherpertz <tillh@live.de>
2022-12-07 22:43:29 +0100

    map...

--- Day 4/day-four.js
+++ Day 4/day-four.js
@@ -17,67 +17,29 @@ elv1 a b
 elv2 c d
 
 pair is only inside if 
-c >= a, d <= b
+a <= c, b >= d
 a >= c, b <= d
-
-True  
-<-a------------b->
-    <-c-----d->
-
-True
-    <-a-----b->
-<-c------------d->
-
-True
-<-a------------b->
-<-c------------d->
-
-
-False
-<-a--b-> 
-        <-c--d->
-
-False
-        <-a--b->
-<-c--d->
-
-False
-    <-a-----b->
-<-c-----d->
-
-False
-<-a-----b->
-    <-c-----d->
-    
-*/
-
-/*
-    Take string up until linebreak
-    convert string up to '-' OR ',' to number
-    first number a, 2nd b, third c, fourth d
 */
 
 // Read Input and create Variables
 const { timeStamp } = require("console");
 const fs = require("fs");
-let input = fs.readFileSync("input.txt").toString().replace(/(\r)/gm, "");
-let inputArray = input.split("");
-const workingArray = [];
-let result = 0;
-let tempString = "";
-let tempArray = [];
-
-let schminput = input.split(/(,|-|\n)/);
-const remove1 = ",";
-const remove2 = "-";
-for (i = 0; i < schminput.length; i++) {
-    if (schminput[i] === remove1 || schminput[i] === remove2) {
-        schminput.splice(i, 1);
-        // break;       //<-- Uncomment  if only the first term has to be removed
-    }
-}
-console.log(schminput);
-
+let inputs = fs.readFileSync("input.txt")
+    .toString()
+    .replace(/\r/g, "")
+    .split('\n');
 // Function
+function part1() {
+    let ans = 0;
+    const map = inputs.map((input) => {
+        const [elv1, elv2] = input
+            .split(",")
+            .map(elv => elv.split("-").map(Number));
 
-// Call Function and Log Result
+        const contains = elv1[0] <= elv2[0] && elv1[1] >= elv2[1] || elv1[0] >= elv2[0] && elv1[1] <= elv2[1];
+        contains ? ans++ : 0;
+    });
+    console.log(ans);
+
+}
+part1()
