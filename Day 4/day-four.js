b2d8e34e86c39817cddcd42b64980381258ddb00
tillherpertz <tillh@live.de>
2022-12-08 09:45:42 +0100

    added day 2 part 2

--- Day 4/day-four.js
+++ Day 4/day-four.js
@@ -24,27 +24,53 @@ a >= c, b <= d
 // Read Input and create Variables
 const { timeStamp } = require("console");
 const fs = require("fs");
-let inputs = fs.readFileSync("input.txt")
+let inputs = fs
+    .readFileSync("input.txt")
     .toString()
     .replace(/\r/g, "")
-    .split('\n');
+    .split("\n");
 // Function
 function part1() {
     let ans = 0;
     const map = inputs.map((input) => {
-        const [elv1, elv2] = input
-            .split(",")
-            .map(elv => elv.split("-").map(Number));
+        // create map with arrow func
+        const [elv1, elv2] = input // create array inside map with elv1 and elv2
+            .split(",") // where the contents are split at ,
+            .map((elv) => elv.split("-").map(Number)); // each substring is split at -
 
-        const contains = elv1[0] <= elv2[0] && elv1[1] >= elv2[1] || elv1[0] >= elv2[0] && elv1[1] <= elv2[1];
+        const contains =
+            (elv1[0] <= elv2[0] && elv1[1] >= elv2[1]) || //a <= c, b >= d
+            (elv1[0] >= elv2[0] && elv1[1] <= elv2[1]); // a >= c, b <= d
         contains ? ans++ : 0;
     });
     console.log(ans);
-
 }
-part1()
+// part1();
+
+// overlaps if part is true and
+// <a----b> a <= c && c <=b
+//      <c----d>
+//<c----d>  c <= a && a <= d
+//     <a----b>
+//
+//
 
 function part2() {
+    let ans = 0;
+    const map = inputs.map((input) => {
+        // create map with arrow func
+        const [elv1, elv2] = input // create array inside map with elv1 and elv2
+            .split(",") // where the contents are split at ,
+            .map((elv) => elv.split("-").map(Number)); // each substring is split at -
 
+        const overlaps =
+            (elv1[0] <= elv2[0] && elv1[1] >= elv2[1]) || //a <= c, b >= d
+            (elv1[0] >= elv2[0] && elv1[1] <= elv2[1]) || // a >= c, b <= d
+            (elv1[0] <= elv2[0] && elv2[0] <= elv1[1]) || // a <= c && c <=b
+            (elv2[0] <= elv1[0] && elv1[0] <= elv2[1]); // c <= a && a <= d
+        overlaps ? ans++ : 0;
+    });
+
+    console.log(ans);
 }
-part2()
+part2();
