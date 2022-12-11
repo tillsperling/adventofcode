c67447564e14c9d117c97451bc874daf136a66e8
tillherpertz <tillh@live.de>
2022-12-11 17:38:04 +0100

    boilerplate solution added, do it myself later

--- Day 7/day-seven.js
+++ Day 7/day-seven.js
@@ -1,24 +1,126 @@
-// Read Input and create Variables
-const { timeStamp } = require("console");
-const fs = require("fs");
-let inputs = fs
+// solution by https://tranzystorek-io.github.io/ 
+// do this myself later
+
+const path = require('path');
+const fs = require('fs');
+
+// read input
+let input = fs
     .readFileSync("input.txt")
     .toString()
     .replace(/\r/g, "")
     .split("\n");
 
-console.log(inputs);
-// Function
-function part1() {
-    // check if cd or ls
-    for (i = 0; i < inputs.length; i++) {
-        if (inputs[i][0] == "$") {
-            if (inputs[i][2] == "c") {
-                console.log("cd");
+// create file constructor
+class File {
+    constructor(name, size) {
+        this.name = name;
+        this._size = size;
+    }
+
+    size() {
+        return this._size;
+    }
+}
+// create directory constructor
+class Dir {
+    constructor(name, parentDir) {
+        this.isDir = true;
+        this.parentDir = parentDir;
+        this.name = name;
+        this.contents = [];
+    }
+
+    // This should be cached, but our input is small enough that its OK to always recompute
+    size(ignore) {
+        if (ignore === this) {
+            return 0;
+        }
+
+        return this.contents.map((v) => v.size(ignore)).reduce((a, b) => a + b, 0);
+    }
+}
+// create filesystem
+class Filesystem {
+    constructor(instructions) {
+        this.rootDir = new Dir('/', null);
+        this.build(instructions);
+    }
+
+    size(ignore) {
+        return this.rootDir.size(ignore);
+    }
+
+    build(instructions) {
+        let currentDir = this.rootDir;
+        for (let line of instructions) {
+            if (line.startsWith('$ cd')) {
+                const [, dir] = /\$ cd (.+)$/.exec(line);
+                if (dir === '/') {
+                    currentDir = this.rootDir;
+                } else if (dir === '..') {
+                    currentDir = currentDir.parentDir;
+                } else {
+                    currentDir = currentDir.contents.find((v) => v.isDir && v.name === dir);
+                }
+            } else if (line.startsWith('$ ls')) {
+                continue;
             } else {
-                console.log("ls");
+                // In a `ls` output
+                if (line.startsWith('dir ')) {
+                    const [, dirName] = /dir (.+)$/.exec(line);
+                    const newDir = new Dir(dirName, currentDir);
+                    currentDir.contents.push(newDir);
+                } else {
+                    // file
+                    let [, size, fileName] = /(\d+) (.+)$/.exec(line);
+                    size = parseInt(size, 10);
+
+                    const newFile = new File(fileName, size);
+                    currentDir.contents.push(newFile);
+                }
             }
         }
     }
+
+    static *walk(dir) {
+        for (let c of dir.contents) {
+            yield c;
+            if (c.isDir) {
+                yield* Filesystem.walk(c);
+            }
+        }
+    }
+
+    *[Symbol.iterator]() {
+        yield* Filesystem.walk(this.rootDir);
+    }
 }
-part1();
+
+const drive = new Filesystem(input);
+const dirs = [...drive].filter((v) => v.isDir);
+
+// Part one
+let smallDirsSum = 0;
+for (let item of dirs) {
+    let size = item.size();
+    if (size <= 100000) {
+        smallDirsSum += size;
+    }
+}
+
+// Part two
+let couldWork = new File('dummy', Number.MAX_VALUE);
+const DRIVE_SIZE = 70000000;
+for (let someDir of dirs) {
+    const sizeWithoutSomeDir = drive.size(someDir);
+    const unusedSpace = DRIVE_SIZE - sizeWithoutSomeDir;
+    if (unusedSpace >= 30000000) {
+        if (someDir.size() < couldWork.size()) {
+            couldWork = someDir;
+        }
+    }
+}
+
+console.log('Part One:', smallDirsSum);
+console.log('Part Two:', couldWork.size());
