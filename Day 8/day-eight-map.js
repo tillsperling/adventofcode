abcd533a2589794ccb9a2e91b1957a40cc6fc2b0
tillherpertz <tillh@live.de>
2022-12-12 16:42:32 +0100

    adding another day8 solution using a map

---
+++ Day 8/day-eight-map.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const inputs = fs
+    .readFileSync("input.txt", { encoding: "utf-8" }) // read day??.txt content
+    .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+    .trim() // Remove starting/ending whitespace
+    .split("\n") // Split on newline
+    .map((input) => [...input].map());
+console.log(inputs);
