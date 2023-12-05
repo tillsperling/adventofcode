5a1e723b1add8ac915eb6643994f17a6d0c4cc19
tillherpertz <tillh@live.de>
2023-12-05 19:00:07 +0100

    2023 day5 mindfuck test just notes

---
+++ 2023/Day 5/Classes/InputConverter.ts
@@ -0,0 +1,21 @@
+//@ts-ignore
+import * as fs from "fs";
+
+export default class InputConverter {
+    input: string;
+    constructor(input: string) {
+        this.input = input;
+    }
+
+    convertToArray(): any[] {
+        const string = fs.readFileSync(this.input).toString("utf-8");
+        const array = string.replace(/\r/g, "").split('\n');
+        // remove empty lines
+        array.forEach((line, index) => {
+            if (line === '') {
+                array.splice(index, 1);
+            }
+        });
+        return array;
+    }
+}
---
+++ 2023/Day 5/main.ts
@@ -0,0 +1,13 @@
+import InputConverter from "./Classes/InputConverter";
+
+const input = new InputConverter('./input.txt').convertToArray();
+
+function solvePart1() {
+    console.log(input);
+}
+function solvePart2() {
+
+}
+
+solvePart1();
+// solvePart2();
