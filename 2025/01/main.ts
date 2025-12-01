07112109d6ab34103c0fe7fc9f353b0c1f9b0696
tillsperling <hi@till-sperling.de>
2025-12-01 13:26:17 +0100

    added 2025/01

---
+++ 2025/01/main.ts
@@ -0,0 +1,68 @@
+//@ts-ignore
+import * as fs from "fs";
+
+interface Rotation {
+    direction: string;
+    amount: number;
+}
+
+const input: string[] = fs.readFileSync('./input.txt', 'utf-8').toString().replace(/\r/g, ' ').split('\n')
+const cords: Rotation[] = [];
+
+for (const element of input) {
+    const rotation: Rotation = {
+        direction: element[0],
+        amount: parseInt(element.slice(1))
+    }
+
+    cords.push(rotation)
+}
+
+const part1 = () => {
+    let dialNumber = 50;
+    let result = 0;
+
+    for(const rotation of cords){
+        if (rotation.direction === 'R') {
+            for (let i = 0; i < rotation.amount; i++) {
+                dialNumber += 1;
+                if (dialNumber > 99) dialNumber = 0;
+            }
+        } else {
+            for (let i = 0; i < rotation.amount; i++) {
+                dialNumber -= 1;
+                if (dialNumber < 0) dialNumber = 99;
+            }
+        }
+
+        if (dialNumber === 0) result++;
+    }
+
+    console.log(result);
+}
+
+const part2 = () => {
+    let dialNumber = 50;
+    let result = 0;
+
+    for (const rotation of cords) {
+        if (rotation.direction === 'R') {
+            for (let i = 0; i < rotation.amount; i++) {
+                dialNumber += 1;
+                if (dialNumber > 99) dialNumber = 0;
+                if (dialNumber === 0) result++;
+            }
+        } else {
+            for (let i = 0; i < rotation.amount; i++) {
+                dialNumber -= 1;
+                if (dialNumber < 0) dialNumber = 99;
+                if (dialNumber === 0) result++;
+            }
+        }
+    }
+
+    console.log(result);
+}
+
+part1()
+part2()
