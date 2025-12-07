77e87942355c62a69bc04b2743f4dc4999519b8c
tillsperling <hi@till-sperling.de>
2025-12-07 10:43:21 +0100

    added 2025/06 part 2

--- 2025/06/main.ts
+++ 2025/06/main.ts
@@ -8,11 +8,12 @@
  * so we have to go through each step and fill the problems step after step
  *
  * map and split at each white space?
-
+ 
  */
 
 
 import * as fs from 'fs';
+import { sumUpArray } from '../../utils/utils';
 
 const input
     = fs.readFileSync('./input.txt', 'utf-8')
@@ -24,21 +25,16 @@ const input
 
 const maxLengths: number[][] = Array.from({ length: input[0].length }, () => [0]);
 
-const input2
-    = fs.readFileSync('./input.txt', 'utf-8')
-    .toString()
-    .replace(/\r/g, ' ');
-
 const part1 = () => {
     const sol = [];
     const problems = createProblems();
-
+    
     for (const problem of problems) {
         let res = 0;
         const operator = problem.pop();
-
+        
         if (operator === '*') res = 1;
-
+        
         for (let i = 0; i < problem.length; i++) {
             if (operator === '*') {
                 res *= problem[i] as number;
@@ -46,22 +42,22 @@ const part1 = () => {
                 res += problem[i] as number;
             }
         }
-
+        
         sol.push(res);
     }
-    // console.log(sumUpArray(sol));
+    console.log(sumUpArray(sol));
 };
 
 const createProblems = () => {
     const problems: string[][] = Array.from({ length: input[0].length }, () => []);
-
+    
     for (let i = 0; i < input[0].length; i++) {
         for (const problem of input) {
             if (problem[i].length > maxLengths[i][0]) maxLengths[i] = [problem[i].length];
             problems[i].push(problem[i]);
         }
     }
-
+    
     return problems.map((problem) =>
         problem.map((element) => {
             if (isNaN(parseInt(element))) {
@@ -80,13 +76,81 @@ console.timeEnd('part1');
 /**
  * PART 2
  *
+ * reparse the input
+ * count amount of columns by amount of operators
  *
  */
 
+const input2 = fs.readFileSync('./input.txt', 'utf-8')
+    .replace(/\r/g, '')
+    .split('\n');
+
+const grid = input2.map(line => line.trim().split(/\s+/));
+const operators = grid.pop();
+input2.pop();
+
+/**
+ * each input line is exactly the same length, we can create our array by just pushing each line in a new array
+ */
+
 const part2 = () => {
-    console.log(input2);
+    const length = input2[0].length;
+    const columns: string[][] = Array.from({ length: length }, () => []);
+    const turnedColumns: string[][][] = Array.from(
+        { length: grid[0].length },
+        () => [],
+    );
+    
+    
+    for (const line of input2) {
+        for (let i = 0; i < length; i++) {
+            columns[i].push(line[i]);
+        }
+    }
+    
+    let index = 0;
+    for (const column of columns) {
+        if (column.some((element) => /\d/.test(element))) {
+            turnedColumns[index].push(column);
+        } else {
+            index++;
+        }
+    }
+    
+    const solutionArr = [];
+    let operatorIndex = 0;
+    for (const problem of turnedColumns) {
+        const numbers = [];
+        for (const subSet of problem) {
+            const numberArr = subSet.filter((element) => /\d/.test(element));
+            const number = turnStringToNumber(numberArr);
+            numbers.push(number);
+        }
+        
+        let sum = 0;
+        const operator = operators![operatorIndex];
+        if (operator === '*') sum = 1;
+        for (const number of numbers) {
+            if (operator === '*') {
+                sum *= number;
+            } else {
+                sum += number;
+            }
+        }
+        solutionArr.push(sum);
+        operatorIndex++;
+    }
+    
+    console.log(sumUpArray(solutionArr));
 };
 
+const turnStringToNumber = (arr: string[]) => {
+    let str = '';
+    for (const char of arr) {
+        str += char;
+    }
+    return parseInt(str);
+};
 
 console.time('part2');
 part2();
